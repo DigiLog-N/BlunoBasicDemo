f0fc7d01e378bfa6cffd8ec5bba5833df02f71ff
jpw-erigo <john.wilson@erigo.com>
2020-09-18 16:55:34 -0400

    Make the BlunoBasicDemo a little fancier by writing the incoming data to the LCD screen

--- Arduino/BlunoBasicDemo/BlunoBasicDemo.ino
+++ Arduino/BlunoBasicDemo/BlunoBasicDemo.ino
@@ -1,13 +1,28 @@
-void setup() {
-    Serial.begin(115200);               //initial the Serial
+#include <Arduino.h>
+#include <SPI.h>
+#include <MultiLCD.h>
+
+LCD_R61581 lcd;
+
+void setup(){
+  lcd.begin();
+  lcd.setFontSize(FONT_SIZE_MEDIUM);  //set font size
+  lcd.setColor(RGB16_RED);  //set strings color
+  lcd.println();
+  lcd.println();
+  lcd.println("TELEMATICS LCD SHIELD V1.0");
+  lcd.println();
+  lcd.setColor(RGB16_WHITE);
+  Serial.begin(115200);               //initial the Serial
 }
 
 void loop()
 {
+    int incomingByte = 0; // for incoming serial data
     if(Serial.available())
     {
-        Serial.write(Serial.read());    //send what has been received
+        incomingByte = Serial.read();
+        lcd.print(char(incomingByte));
+        Serial.write(incomingByte);    //send what has been received
     }
 }
-
-
